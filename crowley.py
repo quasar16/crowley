4c130f749c3d25bee6f22e41cb3f890c6b454745
Alina <alina.mirion@gmail.com>
2020-02-26 21:53:58 +0200

    get important articles based on topic

--- crowley.py
+++ crowley.py
@@ -13,6 +13,7 @@ import re, nltk, spacy
 
 import unicodedata
 import sys
+import json
 
 from gensim.utils import simple_preprocess
 from gensim.corpora.dictionary import Dictionary
@@ -20,7 +21,7 @@ from gensim.corpora.dictionary import Dictionary
 tbl = dict.fromkeys(i for i in range(sys.maxunicode)
                     if unicodedata.category(chr(i)).startswith('P'))
 
-NUM_TOPICS = 20
+NUM_TOPICS = 6
 
 path_to_lda_all = 'ldamodel_all'
 
@@ -61,6 +62,30 @@ def remove_duplicates(x):
     return list(dict.fromkeys(x))
 
 
+def format_topics_sentences(ldamodel, corpus, texts):
+    # Init output
+    sent_topics_df = pd.DataFrame()
+
+    # Get main topic in each document
+    for i, row in enumerate(ldamodel[corpus]):
+        row = sorted(row, key=lambda x: (x[1]), reverse=True)
+        # Get the Dominant topic, Perc Contribution and Keywords for each document
+        for j, (topic_num, prop_topic) in enumerate(row):
+            if j == 0:  # => dominant topic
+                wp = ldamodel.show_topic(topic_num)
+                topic_keywords = ", ".join([word for word, prop in wp])
+                sent_topics_df = sent_topics_df.append(
+                    pd.Series([int(topic_num), round(prop_topic, 4), topic_keywords]), ignore_index=True)
+            else:
+                break
+    sent_topics_df.columns = ['Dominant_Topic', 'Perc_Contribution', 'Topic_Keywords']
+
+    # Add original text to the end of the output
+    contents = pd.Series(texts)
+    sent_topics_df = pd.concat([sent_topics_df, contents], axis=1)
+    return (sent_topics_df)
+
+
 def main(argv):
     if argv.__len__() < 2:
         print("Usage: crowley date output_file")
@@ -84,17 +109,13 @@ def main(argv):
                                          from_param=date,
                                          language='en',
                                          sort_by='relevancy')
-    qm_articles = newsapi.get_everything(q='quantum physics',
-                                         from_param=date,
-                                         language='en',
-                                         sort_by='relevancy')
     all_urls = []
     for article in qc_articles.get('articles'):
         all_urls.append(article.get('url'))
     for article in qp_articles.get('articles'):
         all_urls.append(article.get('url'))
-    for article in qm_articles.get('articles'):
-        all_urls.append(article.get('url'))
+
+    print("All articles ", all_urls.__len__())
 
     # Get content of urls
     all_articles = NewsPlease.from_urls(all_urls)
@@ -104,34 +125,31 @@ def main(argv):
         articles[article.title] = Article(article.image_url, article.url, article.maintext)
 
     # Write urls in file
+    open(outputfile, 'w').truncate(0)
+    json_data = {}
+    json_article = {}
     for article in articles:
 
         art_value = articles[article]
-        try:
-            if art_value.url is not None:
-                open(outputfile, 'a+').write(art_value.url)
-                open(outputfile, 'a+').write("\n")
-            if article is not None:
-                open(outputfile, 'a+').write(article)
-                open(outputfile, 'a+').write("\n")
-            if art_value.image_url is not None:
-                open(outputfile, 'a+').write(art_value.image_url)
-                open(outputfile, 'a+').write("\n")
-            if art_value.maintext is not None:
-                open(outputfile, 'a+').write(art_value.maintext)
-                open(outputfile, 'a+').write("\n\n\n")
-        except OSError:
-            print("Failed to open file to write article %s" % article)
-        else:
+        if art_value.url is not None and article is not None and art_value.image_url is not None and \
+                art_value.maintext is not None:
+            json_article[art_value.url] = art_value.maintext
             print("Successfully wrote article %s " % article)
 
-    words = []
-    with open(outputfile, 'r') as fin:
-        for cnt, line in enumerate(fin):
-            if line != "\n" and not line.startswith("http"):
-                words.append(line)
+    json_data['content'] = json_article
+    try:
+        open(outputfile, 'w').write(json.dumps(json_data))
+    except OSError:
+        print("Failed to open file to write article %s" % article)
 
-    words_tokenized = tokenize(words)
+    df = pd.read_json(outputfile)
+    df.head()
+    data = df.content.values.tolist()
+    data = [re.sub('\S*@\S*\s?', '', sent) for sent in data]
+    data = [re.sub('\s+', ' ', sent) for sent in data]
+    data = [re.sub("\'", "", sent) for sent in data]
+
+    words_tokenized = tokenize(data)
 
     stop_words = stopwords.words('english')
     stop_words.extend(['from', 'subject', 're', 'edu', 'use', 'to', 'the', 'of', 'a', 'and', 'that', 'in', 'is', 'can',
@@ -152,14 +170,40 @@ def main(argv):
             corpus=corpus_all, num_topics=NUM_TOPICS, id2word=dictionary_all,
             update_every=5, chunksize=10000, passes=1)
         ldamodel_all.save(path_to_lda_all)
-    print("Done training models. Saved them on disk.")
+        print("Done training models. Saved them on disk.")
+
+        df_topic_sents_keywords = format_topics_sentences(ldamodel=ldamodel_all, corpus=corpus_all,
+                                                          texts=without_stopwords)
+
+        # Format
+        df_dominant_topic = df_topic_sents_keywords.reset_index()
+        df_dominant_topic.columns = ['Document_No', 'Dominant_Topic', 'Topic_Perc_Contrib', 'Keywords', 'Text']
+
+        # Show
+        print(df_dominant_topic.head(5))
+
+        # Group top 5 sentences under each topic
+        sent_topics_sorteddf_mallet = pd.DataFrame()
+
+        sent_topics_outdf_grpd = df_topic_sents_keywords.groupby('Dominant_Topic')
+
+        for i, grp in sent_topics_outdf_grpd:
+            sent_topics_sorteddf_mallet = pd.concat([sent_topics_sorteddf_mallet,
+                                                     grp.sort_values(['Perc_Contribution'], ascending=[0]).head(1)],
+                                                    axis=0)
+
+        # Reset Index
+        sent_topics_sorteddf_mallet.reset_index(drop=True, inplace=True)
+
+        # Format
+        sent_topics_sorteddf_mallet.columns = ['Topic_Num', "Topic_Perc_Contrib", "Keywords", "Text"]
+
+        # Show
+        print(sent_topics_sorteddf_mallet.head())
 
     all_topics = get_topics(ldamodel=ldamodel_all, num_words=10)
     for i, topic in enumerate(all_topics):
         print(i, topic)
 
-    # print(topics)
-    # topics = remove_duplicates(topics)
-    # print(topics)
 
 main(sys.argv[1:])
